02dca93b4bb5899d525ed3a27698e1bd53fe7415
David Hicks <dlh@es.aau.dk>
2019-02-27 02:37:04 +0100

    this is an update adding a TextView to the UI for main Activity.

--- app/src/main/java/com/example/dlh/hw_version33/MainActivity.java
+++ app/src/main/java/com/example/dlh/hw_version33/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static final String MY_CONSTANT = "myConstant";
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
